2bc7f4c1c5c587ef29b3483ff1ce18c64f1ac921
NKenzhebekov <nurlan.kenzhebekov@gmail.com>
2018-12-29 11:34:01 +0600

    minor changes

--- src/main/java/kz/kartel/dutyscheduler/security/WebSecurity.java
+++ src/main/java/kz/kartel/dutyscheduler/security/WebSecurity.java
@@ -31,6 +31,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .antMatchers(HttpMethod.GET, "/dutiesByDate").permitAll()
+                .antMatchers(HttpMethod.POST, "/commentsByDuty").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
